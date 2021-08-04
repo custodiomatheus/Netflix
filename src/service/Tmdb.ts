3dc7510beb437c66981ed1163be9fac7a48e5b72
custodiomatheus <matheuscustodio76@gmail.com>
2021-08-03 22:45:28 -0300

    feat(tmdb): tmdb uris

---
+++ src/service/Tmdb.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const findSeries = async (uri: string) => {
+  const response = await axios.get(`https://api.themoviedb.org/3/${uri}`);
+  return response.data.results;
+}
+
+export default {
+  getHomeList: async () => {
+    return [
+      {
+        title: "Em Alta",
+        genres: "top_rated",
+        items: await findSeries(`movie/top_rated?api_key=${process.env.VUE_APP_ROOT_API_KEY}&language=pt-BR`)
+      },
+
+      {
+        title: "Originais netflix",
+        genres: "originals",
+        items: await findSeries(`discover/tv?api_key=${process.env.VUE_APP_ROOT_API_KEY}&with_network=213&language=pt-BR`)
+      },
+
+      {
+        title: "Recomendados",
+        genres: "trending",
+        items: await findSeries(`trending/all/week?api_key=${process.env.VUE_APP_ROOT_API_KEY}&language=pt-BR`)
+      },
+
+      {
+        title: "Ficção",
+        genres: "fiction",
+        items: await findSeries(`discover/tv?api_key=${process.env.VUE_APP_ROOT_API_KEY}&without_genres=10765&language=pt-BR`)
+      },
+
+      {
+        title: "Ação",
+        genres: "action",
+        items: await findSeries(`discover/tv?api_key=${process.env.VUE_APP_ROOT_API_KEY}&without_genres=10759&language=pt-BR`)
+      }
+    ]
+  }
+}
