7133b3522aca397d986175398d14d20ddf804eca
custodiomatheus <matheuscustodio76@gmail.com>
2021-08-03 22:43:26 -0300

    feat(main): dotenv configuration

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import dotenv from "dotenv";
+dotenv.config({
+  path: `../.env.${process.env.NODE_ENV}.local`
+})
 
 createApp(App).use(store).use(router).mount("#app");
