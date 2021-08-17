95f698c6ce6ed4c7294f950a53a9e712a73098d6
custodiomatheus <matheuscustodio76@gmail.com>
2021-08-16 23:54:42 -0300

    feat(UserCard): UserCard Component

---
+++ src/components/UserCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="user" @click="selectedUser">
+    <div class="user--card"></div>
+    <p class="user--nickname">aaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class UserCard extends Vue {
+  selectedUser() {
+    this.$router.push("/home");
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.user {
+  text-align: center;
+  word-break: break-all;
+  width: 165px;
+  cursor: pointer;
+
+  &:hover &--card {
+    border: 3px solid #c4c4c4;
+  }
+
+  &:hover &--nickname {
+    color: #c4c4c4;
+  }
+
+  &--card {
+    width: 100%;
+    height: 170px;
+    border-radius: 5px;
+    background-color: rgb(86, 86, 129);
+  }
+
+  &--nickname {
+    font-size: 18px;
+    color: gray;
+  }
+
+}
+</style>
