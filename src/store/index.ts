89af1330c4c9b64173bff327e8c2312614ae80bf
custodiomatheus <matheuscustodio76@gmail.com>
2021-08-21 01:56:18 -0300

    feat(index): using vuex-persist to account

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,18 @@
 import { createStore } from "vuex";
+import VuexPersistence from "vuex-persist";
+
+const vuexLocal = new VuexPersistence({
+  reducer: (state: any) => ({
+    account: state.account,
+  }),
+  storage: window.sessionStorage,
+});
+
+import account from "./modules/account";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    account,
+  },
+  plugins: [vuexLocal.plugin],
 });
