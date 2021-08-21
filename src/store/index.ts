310e91717baf7d879a8b6c9c057128d484d3eacc
custodiomatheus <matheuscustodio76@gmail.com>
2021-08-21 17:12:10 -0300

    feat(store): user added to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,21 @@
 import { createStore } from "vuex";
 import VuexPersistence from "vuex-persist";
 
+import account from "./modules/account";
+import user from "./modules/user";
+
 const vuexLocal = new VuexPersistence({
   reducer: (state: any) => ({
     account: state.account,
+    user: state.user
   }),
   storage: window.sessionStorage,
 });
 
-import account from "./modules/account";
-
 export default createStore({
   modules: {
     account,
+    user,
   },
   plugins: [vuexLocal.plugin],
 });
