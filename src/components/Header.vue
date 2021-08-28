1de4541c7f1ed7f6861553fe9b2c12bd6a292d76
custodiomatheus <matheuscustodio76@gmail.com>
2021-08-28 17:48:25 -0300

    feat(Header): change and search shows

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,44 +8,120 @@
         : 'none',
     ]"
   >
-    <img
-      src="https://image.tmdb.org/t/p/w300/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"
-      :height="$route.path === '/login' ? 50 : 32"
-      alt=""
-    />
+    <div style="display: flex; align-items: center">
+      <img
+        src="https://image.tmdb.org/t/p/w300/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"
+        :height="$route.path === '/login' ? 50 : 32"
+        alt=""
+      />
 
-    <nav class="primary-nav" v-if="$route.path === '/home'">
-      <ul class="primary-nav-list">
-        <li class="primary-nav-list__item active">Início</li>
-        <li>Séries</li>
-        <li>Filmes</li>
-        <li>Bombando</li>
-        <li>Minha Lista</li>
-      </ul>
+      <nav
+        class="primary-nav"
+        v-if="['/search', '/home'].includes($route.path)"
+      >
+        <ul class="primary-nav-list">
+          <li
+            :class="getShowType === 'home' ? 'active' : ''"
+            @click="handlePage('home')"
+          >
+            Início
+          </li>
+          <li
+            :class="getShowType === 'series' ? 'active' : ''"
+            @click="handlePage('series')"
+          >
+            Séries
+          </li>
+          <li
+            :class="getShowType === 'movies' ? 'active' : ''"
+            @click="handlePage('movies')"
+          >
+            Filmes
+          </li>
+          <li>Minha Lista</li>
+        </ul>
+      </nav>
+    </div>
+
+    <nav
+      class="secondary-nav"
+      v-if="['/search', '/home'].includes($route.path)"
+    >
+      <input
+        type="search"
+        class="input-search"
+        id="input-search"
+        v-model="searchShow"
+      />
+      <i class="material-icons search-icon" @click="search">search</i>
     </nav>
   </header>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import { mapActions, mapGetters } from "vuex";
 
-Options({
+@Options({
   data() {
     return {
       topDistance: 0,
+      searchShow: "",
     };
   },
-});
-
+  computed: {
+    ...mapGetters("show", ["getShowType"]),
+  },
+  methods: {
+    ...mapActions("show", ["ActionSetShowType"]),
+  },
+  watch: {
+    searchShow() {
+      if (this.searchShow) {
+        this.$router.push({ path: "/search", query: { q: this.searchShow } });
+      } else {
+        this.$router.push("/home");
+        document
+          .getElementById("input-search")
+          ?.classList.remove("input-search-focus");
+      }
+    },
+  },
+})
 export default class Header extends Vue {
   topDistance = 0;
+  getShowType!: string;
+  searchShow!: string;
+  ActionSetShowType!: (page: string) => void;
 
   created(): void {
     window.addEventListener("scroll", this.handleScroll);
   }
 
+  mounted() {
+    this.validateSeachShow();
+  }
+
+  validateSeachShow() {
+    if (this.$route.path === "/search" && !this.searchShow) {
+      this.$router.push("/home");
+      document.getElementById("input-search")?.blur();
+    }
+  }
+
   handleScroll(): void {
     this.topDistance = window.pageYOffset;
+  }
+
+  handlePage(page: string): void {
+    this.ActionSetShowType(page);
+    this.$emit("handlePage");
+  }
+
+  search() {
+    document
+      .getElementById("input-search")
+      ?.classList.add("input-search-focus");
   }
 }
 </script>
@@ -63,11 +139,13 @@ header {
   width: 100%;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 3.5vh 62px;
   position: fixed;
   top: 0;
   z-index: 2;
   transition: background-color 0.5s;
+  // background-color: green;
 
   h1 {
     font-size: 32px;
@@ -103,6 +181,41 @@ header {
           color: var(--gray);
         }
       }
+    }
+  }
+
+  nav.secondary-nav {
+    height: 35px;
+    display: flex;
+    align-items: center;
+    position: relative;
+
+    .search-icon {
+      position: absolute;
+      left: 10px;
+      font-size: 28px;
+    }
+
+    .input-search {
+      width: 0;
+      height: inherit;
+      color: var(--white);
+      padding-left: 45px;
+      background-color: transparent;
+      border: 0 solid var(--white);
+      transition: all 0.2s ease;
+    }
+
+    // .input-search:focus {
+    //   width: 260px;
+    //   border-width: 1px;
+    //   background-color: var(--black);
+    // }
+
+    .input-search-focus {
+      width: 260px;
+      border-width: 1px;
+      background-color: var(--black);
     }
   }
 }
