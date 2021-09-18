753f1eb03802d29ffc5c73ac5be7b175d0aa51e9
custodiomatheus <matheuscustodio76@gmail.com>
2021-09-18 19:16:14 -0300

    feat(ShowCard): add favorite

--- src/components/ShowCard.vue
+++ src/components/ShowCard.vue
@@ -1,48 +1,118 @@
 <template>
   <li :class="$route.path === '/home' ? 'card-home' : 'card-search'">
-    <img :src="imageUrl" :alt=serie.original_title />
+    <span class="material-icons card-icon" @click="addFavorite"
+      >add_circle_outline</span
+    >
+    <img class="card-image" :src="imageUrl" :alt="serie.original_title" />
   </li>
 </template>
 
 <script lang="ts">
 import { Vue, Options } from "vue-class-component";
+import { mapGetters } from "vuex";
+
+import api from "../service/api";
 
 @Options({
   props: {
+    type: {
+      type: String,
+      require: true,
+    },
     serie: {
       type: Object,
       require: true,
     },
   },
+  computed: {
+    ...mapGetters("user", ["getUserId"]),
+  },
 })
 export default class ShowCard extends Vue {
+  type!: string;
   serie!: any;
+  getUserId!: number;
 
   get imageUrl(): string {
-    if (this.serie?.poster_path || this.serie?.backdrop_path) {
-      return `https://image.tmdb.org/t/p/w300${
-        this.$route.path === "/home"
-          ? this.serie.poster_path
-          : this.serie.backdrop_path
-      }`;
-    } else {
-      return "https://occ-0-759-185.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABTYctxxbe-UkKEdlMxXm4FVGD6DqTHkQ0TQ5CQJ9jbOMnG0CYxYcSICcTUQz8DrB7CpKUGpqJVMtEqksLlvSJx2ac3Ak.png?r=a41";
-    }
+    return this.serie?.backdrop_path
+      ? `https://image.tmdb.org/t/p/w500/${this.serie.backdrop_path}`
+      : "https://occ-0-759-185.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABTYctxxbe-UkKEdlMxXm4FVGD6DqTHkQ0TQ5CQJ9jbOMnG0CYxYcSICcTUQz8DrB7CpKUGpqJVMtEqksLlvSJx2ac3Ak.png?r=a41";
+  }
+
+  addFavorite(): void {
+    console.log(this.serie);
+    
+    api
+      .put(`/users-shows`, {
+        user: {
+          id: this.getUserId,
+        },
+        show: {
+          id: this.serie.id,
+          type: this.type || this.serie.media_type,
+        },
+        is_favorite: true,
+        is_watched: false,
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  addWatched(): void {
+    api
+      .put(`/users-shows`, {
+        user: {
+          id: this.getUserId,
+        },
+        show: {
+          id: this.serie.id,
+        },
+        is_favorite: false,
+        is_watched: true,
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.card-home,
+.card-search {
+  position: relative;
+}
+
+.card-icon {
+  position: absolute;
+  right: 5px;
+  top: 5px;
+  font-size: 26px;
+  z-index: 1;
+  display: none;
+}
+
 .card-home {
-  width: 150px;
-  height: 225px;
-  background-color: var(--gray);
+  width: 350px;
+  height: 195px;
   cursor: pointer;
-  transform: scale(0.9);
+  transform: scale(0.96);
   transition: all ease 0.2s;
 
   &:hover {
     transform: scale(1);
+  }
+
+  &:hover img {
+    filter: brightness(30%);
+  }
+
+  &:hover .card-icon {
+    display: flex;
   }
 
   img {
@@ -60,6 +130,14 @@ export default class ShowCard extends Vue {
 
   &:hover {
     transform: scale(1);
+  }
+
+  &:hover img {
+    filter: brightness(30%);
+  }
+
+  &:hover .card-icon {
+    display: flex;
   }
 
   img {
