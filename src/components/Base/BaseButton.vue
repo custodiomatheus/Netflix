dea1e1ff5d1ea861db30bb4eb24432d287b5f11a
custodiomatheus <matheuscustodio76@gmail.com>
2024-01-18 20:53:05 -0300

    NF: new BaseButton component

---
+++ src/components/Base/BaseButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button>{{ text }}</button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  text: string;
+}
+
+const props = defineProps<Props>();
+</script>
+<style lang="scss" scoped>
+button {
+  width: 100%;
+  background-color: $red;
+  padding: 16px;
+  border-radius: $border-radius;
+  border: none;
+  color: $white;
+  font-size: 1rem;
+}
+</style>
