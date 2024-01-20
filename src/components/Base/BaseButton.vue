b0c0e9a5e20cfdd93340a13ee6529ff9c22f323b
custodiomatheus <matheuscustodio76@gmail.com>
2024-01-19 21:02:02 -0300

    IMP: added loading animation

--- src/components/Base/BaseButton.vue
+++ src/components/Base/BaseButton.vue
@@ -1,13 +1,19 @@
 <template>
-  <button>{{ text }}</button>
+  <button>
+    <div :class="{ loading: loading }" />
+    {{ text }}
+  </button>
 </template>
 
 <script setup lang="ts">
 interface Props {
   text: string;
+  loading?: boolean;
 }
 
-const props = defineProps<Props>();
+withDefaults(defineProps<Props>(), {
+  loading: false,
+});
 </script>
 <style lang="scss" scoped>
 button {
@@ -18,5 +24,19 @@ button {
   border: none;
   color: $white;
   font-size: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  .loading {
+    width: 20px;
+    height: 20px;
+    border: 2px solid transparent;
+    border-radius: 50%;
+    border-top: 2px solid $white;
+    animation: spin 1s linear infinite;
+    margin: 0 7px 0 -20px;
+  }
 }
 </style>
