52c017f59b007792ad5894f58ede932ddffdb4eb
custodiomatheus <matheuscustodio76@gmail.com>
2024-02-02 22:25:03 -0300

    IMP: update urls

--- src/components/Header/HeaderAuth.vue
+++ src/components/Header/HeaderAuth.vue
@@ -1,11 +1,11 @@
 <template>
   <header>
-    <img :src="`${TMDB_URL}w300/wwemzKWzjKYJFfCeiB57q3r4Bcm.png`" alt="Logo" height="50" />
+    <img :src="`${TMDB_IMAGE_URL}w300/wwemzKWzjKYJFfCeiB57q3r4Bcm.png`" alt="Logo" height="50" />
   </header>
 </template>
 
 <script setup lang="ts">
-import { TMDB_URL } from "@/helpers/constants/urls";
+import { TMDB_IMAGE_URL } from "@/helpers/constants/urls";
 </script>
 
 <style lang="scss" scoped>
--- src/helpers/constants/urls.ts
+++ src/helpers/constants/urls.ts
@@ -1,1 +1,2 @@
-export const TMDB_URL = "https://image.tmdb.org/t/p/";
+export const TMDB_API_URL = "https://api.themoviedb.org/3/";
+export const TMDB_IMAGE_URL = "https://image.tmdb.org/t/p/";
